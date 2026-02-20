fa4368874a3d9dcfbb887aed631bfd3720423f3d
Anton Khamets <anton.khamets@hey.com>
2026-02-21 04:49:01 +0500

    Make ball roll!

--- player.gd
+++ player.gd
@@ -1,31 +1,43 @@
-extends CharacterBody3D
+extends RigidBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
+var start_position: Vector3
+var start_rotation: Basis
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	start_position = global_position
+	start_rotation = global_transform.basis
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("reset"):
+		global_position = start_position
+		global_transform.basis = start_rotation
+		linear_velocity = Vector3.ZERO
+		angular_velocity = Vector3.ZERO
+		target_velocity = Vector3.ZERO
+
+func _physics_process(_delta: float) -> void:
 	var direction = Vector3.ZERO
 	
 	if Input.is_action_pressed("move_right"):
-		direction.z -= 1
-	if Input.is_action_pressed("move_left"):
-		direction.z += 1
-	if Input.is_action_pressed("move_back"):
-		direction.x += 1
-	if Input.is_action_pressed("move_forward"):
 		direction.x -= 1
+	if Input.is_action_pressed("move_left"):
+		direction.x += 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
 		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
 		
+	# direction = global_transform.basis * direction
+		
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
 	
-	if not is_on_floor():
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-		
-	velocity = target_velocity
-	move_and_slide()
+	angular_velocity = target_velocity
