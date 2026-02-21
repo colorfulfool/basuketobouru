47ba13463ade7e40d028089fe924adf90241f603
Anton Khamets <anton.khamets@hey.com>
2026-02-21 05:14:35 +0500

    Add jumping

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 
 @export var speed = 14
+@export var jump_impulse = 5
 @export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
@@ -27,9 +28,11 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("move_left"):
 		direction.x += 1
 	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z += 1
+	if Input.is_action_just_released("jump"):
+		linear_velocity.y = jump_impulse
 		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
